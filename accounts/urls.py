0a414ea8c5b129fc86f8ec8506db965e195ff009
bichana <bichana>
2020-04-14 03:01:19 +0000

    added userdetail system.

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,8 +13,8 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-	#path("profile/<int:pk>",views.UserDetail.as_view(),name ="user_detail"),
-	#path("profile/update/<int:pk>",views.UserUpdate.as_view(),name = "user_update"),
+	path("profile/<int:pk>",views.UserDetail.as_view(),name ="user_detail"),
+	path("profile/update/<int:pk>",views.UserUpdate.as_view(),name = "user_update"),
 	#path("password_change/",views.PasswordChangeView.as_view(),name = "password_change"),
 	#path("password_change/done/",views.PasswordChangeDoneView.as_view(),name = "password_change_done"),
 ]
--- project2/urls.py
+++ project2/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("expensess.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
